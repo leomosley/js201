c0d23a0c58b97cb08869e1e83c6ebfbaacf3e9b2
leomosley <leo.mosley2@gmail.com>
2024-09-23 14:04:27 +0100

    completed, all tests pass

--- index.js
+++ index.js
@@ -14,19 +14,110 @@
 
 function id() {
   // e.g. return "UP654321";
+  return "UP2212140";
 }
 
 function fn() {
-  return 'Replace This With Your First Name';
+  // return 'Replace This With Your First Name';
+  return 'Leo';
 }
 
 function sn() {
-  return 'Replace This With Your Surname';
+  // return 'Replace This With Your Surname';
+  return 'Mosley';
 }
 
-function example() {
-  // replace this example with
-  // your first function then
-  // add more below as necessary.
+function add(a, b) {
+  return a + b;
+}
 
+function subtract(a, b) {
+  return a - b;
+}
+
+function checkObject(obj) {
+  obj.checked = true;
+}
+
+function checkObjectInside(obj) {
+  if (obj.data) obj.data.checked = true;
+}
+
+function arraySet(arr, i, n) {
+  if (arr[i]) arr[i] = n;
+}
+
+function addAll(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+function larger(a, b) {
+  return a > b ? a : b;
+}
+
+function largest(arr) {
+  let max = arr[0] ?? null;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) max = arr[i];
+  }
+  return max;
+}
+
+function compare(a, b) {
+  if (a.length !== b.length) return false;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
+
+function addToAll(arr, n) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] += n;
+  }
+  return arr;
+}
+
+let remembered;
+
+function rememberThis(keepsake) {
+  remembered = keepsake;
+}
+
+function nArray(n) {
+  let arr = [];
+  for (let i = 1; i <= n; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+function addAllOpt(arr) {
+  if (!arr) return 0;
+
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+function divisors(arr, div) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % div === 0) newArr.push(arr[i]);
+  }
+  return newArr;
+}
+
+function multiples(n, m) {
+  let arr = [];
+  for (let i = 1; i <= n; i++) {
+    arr.push(i * m);
+  }
+  return arr;
 }
